2d2c38ecd773e105116a91229d1cf4dfaac1b043
Fredrik Silvearv <frsi@svk.se>
2021-09-20 13:15:59 +0200

    Add rdfgraph.py

--- cimsparql/rdfgraph.py
+++ cimsparql/rdfgraph.py
@@ -10,9 +10,12 @@ import requests
 from cimsparql.model import CimModel
 
 class RdflibGraph(CimModel):
+    def __init__(mapper: CimModel, infer: bool = False, sameas: bool = True) -> None:
+        super().__init__(mapper, infer, sameas)
 
-    def GraphParse():
+    def GraphParse() -> Graph:
         graph = Graph()
         graph.parse("/home/frsi/abot/rdfparser/graph_output.txt")
+        return graph
 
 
